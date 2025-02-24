2afa9e531a89e9ba923848725769a2f56fbad06b
Joy <philip.joy@dfg.alaska.local>
2025-02-23 16:33:05 -0900

    Sc mortality and weight data

---
+++ scripts/mort_weight_data_exam.R
@@ -0,0 +1,21 @@
+################################################################################
+# Checking out mortality and weight data from regions:
+################################################################################
+library(readxl)
+library(janitor)
+library(ggplot2)
+library(tidyverse)
+library(tidyr)
+library(wesanderson)
+library(janitor)
+###############################################################################
+dat <- read.csv("data/raw_dat/2023/rf_mort_sc.csv") %>%
+  clean_names() %>% mutate(rel_cat = as.factor(rel_cat))
+
+ggplot(dat, aes(x = year, y = mort_rate, col = assemblage, shape = rel_cat)) +
+  geom_point() +
+  facet_wrap(~cfmu + user)
+
+ggplot(dat, aes(x = year, y = p_rel, col = assemblage, shape = rel_cat)) +
+  geom_point() +
+  facet_wrap(~cfmu + user)
