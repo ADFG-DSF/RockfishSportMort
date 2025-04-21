22101ba998699cb9d8f27785c3bc04ca56585d6f
Joy <philip.joy@dfg.alaska.local>
2025-04-21 07:31:10 -0800

    Bayes momdel update. Minor tweak and ready to add biomass and mortality estimates

--- scripts/run_bayes_FITmod_2b_3pHspec_infP.R
+++ scripts/run_bayes_FITmod_2b_3pHspec_infP.R
@@ -68,7 +68,7 @@ mod <- "HR_fitLBR_2bias_hierPcomp_3pH_hybPr_splitpH_v3"
 mod <- "HR_fitLBR_2bias_hierPcomp_3pH_hybPr_splitpH_v4"
 mod <- "HR_hybLBR_2bias_hierPcomp_3pH_hybPr_splitpH_v4"
 mod <- "HR_fitLBR_2bias_hierPcomp_3pH_hybPr_splitpH_v5"
-mod <- "HR_hybLBR_2bias_hierPcomp_3pH_CLEAN"
+mod <- "HR_hybLBR_2bias_hierPcomp_3pH_CLEAN2"
 mod <- "HR_fitLBR_2bias_hierPcomp_3pH_CLEAN"
 
 if (mod <- "HR_hybLBR_2bias_hierPcomp_3pH_hybPr_splitpH_v4") {
@@ -326,7 +326,7 @@ saveRDS(postH, paste0(".\\output\\bayes_posts\\",mod,"_thru",end_yr,"_",ni,"_",o
 saveRDS(postH, paste0("H:\\Documents\\Rockfish_SF_mortality\\RockfishSportMort\\output\\bayes_posts\\",mod,"_thru",end_yr,"_",ni,"_",Sys.Date(),".rds"))
 #-------------------------------------------------------------------------------
 # Or are we just re-examinng a past run? See /output/bayes_posts/ folder
-results <- "HR_hybLBR_2bias_hierPcomp_3pH_hybPr_splitpH_v4_thru2023_5e+06_2025-04-15"
+results <- "HR_fitLBR_2bias_hierPcomp_3pH_CLEAN_thru2023_3e+06_7kn_2025-04-18"
 
 #model_HCR_censLBR_xspline_thru2019_6e+06_2024-11-24; 98% converged
 #model_HCR_censLBR_1bc_xspline_thru2019_6e+06_2024-11-24; 99% converged
