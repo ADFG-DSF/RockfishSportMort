bb0644103448be5c01d7fdb1ea65e1d25615ffa2
Joy <philip.joy@dfg.alaska.local>
2025-09-10 09:19:01 -0800

    model corrections and updates

--- scripts/run_bayes_working.R
+++ scripts/run_bayes_working.R
@@ -52,14 +52,16 @@ area_codes <- comp %>% select(area,area_n) %>% unique() %>%
 # Run models!
 
 #iterations, burnin, chains and trimming rate:
-ni <- 50E5; nb <- ni*.5; nc <- 3; nt <- (ni - nb) / 1000
+ni <- 10E5; nb <- ni*.5; nc <- 3; nt <- (ni - nb) / 1000
 # 15e5 = 1.6 - 1.7 days
 # 25e5 = 2.9 days
 
 #model to run; see /models folder
 mod <- "Gen3aa_indcomp_pH33flat_no_swhs_rel_FULL" #at 15e5, second half of trace plots look converged, pH_1 may need tightening; p_black may need rethinking on hyper priors to align with inside/outside rather than regions?
 mod <- "Gen3aa_indcomp_pH33B2share_no_swhs_rel_FULL"
+mod <- "Gen3aa_indcomp_pH33B2share_swhsR_FULL"
 mod <- "Gen3aa_indcomp_no_swhs_rel_FULL"
+mod <- "Gen3aa_indcomp_swhsR_FULL"
 
 #if (mod <- "HR_hybLBR_2bias_hierPcomp_3pH_hybPr_splitpH_v4") {
 #  jags_dat$Rlbp_ayg[jags_dat$Rlbp_ayg == 0] <- 1
@@ -69,9 +71,9 @@ mod <- "Gen3aa_indcomp_no_swhs_rel_FULL"
 #Are we using starting values from a prior model?
 use_inits = "yes"
 
-use_this_model <- "Gen3aa_indcomp_pH33flat_no_swhs_rel_thru2023_2500000__2025-08-22"
-use_this_model <- "Gen3aa_indcomp_pH33B2share_no_swhs_rel_thru2023_2500000_2025-08-22"
-use_this_model <- "Gen3aa_indcomp_thru2023_2500000__2025-08-17"
+use_this_model <- "Gen3aa_indcomp_pH33flat_no_swhs_rel_FULL_thru2023_5e+06_2025-08-25"
+use_this_model <- "Gen3aa_indcomp_pH33B2share_no_swhs_rel_FULL_thru2023_5e+06_2025-08-26"
+use_this_model <- "Gen3aa_indcomp_no_swhs_rel_FULL_thru2023_5e+06_2025-08-26"
 
 initspost <- readRDS(paste0(".\\output\\bayes_posts\\",use_this_model,".rds"))
 
@@ -355,11 +357,11 @@ other_label <- paste0(jags_dat$C,"kn")
 other_label <- ""
 
 saveRDS(postH, paste0(".\\output\\bayes_posts\\",mod,"_thru",end_yr,"_",ni,"_",other_label,"_",Sys.Date(),".rds"))
-saveRDS(postH, paste0("E:\\FSI backup files\\Rockfish_SF_mortality\\RockfishSportMort\\output\\bayes_posts\\",mod,"_thru",end_yr,"_",ni,"_",Sys.Date(),".rds"))
+#saveRDS(postH, paste0("E:\\FSI backup files\\Rockfish_SF_mortality\\RockfishSportMort\\output\\bayes_posts\\",mod,"_thru",end_yr,"_",ni,"_",Sys.Date(),".rds"))
 saveRDS(postH, paste0("H:\\Documents\\Rockfish_SF_mortality\\RockfishSportMort\\output\\bayes_posts\\",mod,"_thru",end_yr,"_",ni,"_",Sys.Date(),".rds"))
 #-------------------------------------------------------------------------------
 # Or are we just re-examinng a past run? See /output/bayes_posts/ folder
-results <- "Gen3aa_indcomp_no_swhs_rel_FULL_thru2023_5e+06__2025-08-26"
+results <- "Gen3aa_indcomp_pH33B2share_no_swhs_rel_FULL_thru2023_5e+06_2025-08-26"
 
 #model_HCR_censLBR_xspline_thru2019_6e+06_2024-11-24; 98% converged
 #model_HCR_censLBR_1bc_xspline_thru2019_6e+06_2024-11-24; 99% converged
@@ -551,13 +553,14 @@ jagsUI::traceplot(postH, parameters = c("sd_pH","mu_beta0_pH","tau_beta0_pH",
                                         "mu_beta4_pH","tau_beta4_pH",
                                         "mu_beta5_pH","tau_beta5_pH"))
 
+jagsUI::traceplot(postH, parameters = c("beta2_pH"))
 jagsUI::traceplot(postH, parameters = c("beta4_pH"))
 
 jagsUI::traceplot(postH, parameters = c("beta0_pH","beta1_pH",
                                         "beta2_pH","beta3_pH",
                                         "beta5_pH","beta6_pH"))
 
-jagsUI::traceplot(postH, parameters = "beta6_pH")
+jagsUI::traceplot(postH, parameters = "tau_prigui_pre")
 
 rhat_exam %>% group_by(variable,area) %>%
   summarise(n = n(),
